5183f4d95199d8682b4746b426fff7ef24208922
bobbyworld88 <70490222+bobbyworld88@users.noreply.github.com>
2020-10-28 23:29:45 -0500

    Olympics

--- week1/grocery.js
+++ week1/grocery.js
@@ -1,16 +1,19 @@
 var shopper = {
+    //String//
     shopperName: "Robert",
+    //Number//
     grocerylist: 6,
+    //Boolean//
     isPaid: false,
+    //Array//
     groceryCart: ["cheese", "chicken", "rice", "spinach"],
-
+    //Function - customer name with cart items and total item count//
     checkout: function() {
         console.log(`${this.shopperName} has this following groceries in their basket: ${shopper.groceryCart}. Total items purchased: ${this.grocerylist}`);
     }
 
-
 };
 
 shopper.checkout();
-//DRY don't repeat yourself - he will ask in week3 //
 
+//DRY don't repeat yourself - he will ask in week3 //
---
+++ week2/olympics.js
@@ -0,0 +1,83 @@
+//Preliminaries//
+/*
+if (5 > 3) {
+    console.log("is greater than");
+}
+
+if (cat = 3) {
+    console.log("is the length");
+}
+
+if ("cat" === "dog") {
+    console.log(" ");
+}
+else {
+    console.log("not the same");
+}
+*/
+/*
+//Bronze Medal//
+
+const person = {
+    name: "Bobby",
+    age: 18,
+}
+
+//Are you old enough for the movies//
+
+if (person >= 18 ) {
+    console.log(`${person.name} is allowed to go to the movie if they are old enough`);
+}
+
+else {
+    console.log(`${person.name} is not allowed to go to the movie since they are not old enough`);
+}
+
+
+//Only allow names that start with "B"//
+
+const firstLetter = "Bobby";
+ console.log(firstLetter.charAt(0));
+
+//Only allow name that starts with "B" and they are older than 18//
+
+if (person.age = 18 && firstLetter) {
+    console.log(`${person.name} is allowed into the movies because his name starts with a ${firstLetter.charAt(0)}`);
+}
+
+else {
+    console.log("ERROR 54");
+}
+*/
+
+
+
+//Silver Medal//
+
+// if / else if / else problem//
+
+if (1 === 1) {
+    console.log("strict");
+}
+
+else if ("1" == 1){
+    console.log("loose");
+}
+
+else if (1 == "1") {
+    console.log("abstract");
+}
+
+else {
+    console.log("not equal at all")
+}
+
+// 
+
+if (1 <= 2 && 2 === 4) {
+    console.log("yes");
+}
+
+else {
+    console.log("no");
+}
